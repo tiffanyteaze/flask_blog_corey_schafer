17aa147b0bbbba8c1a61e981e3d32677d8c509cb
tiffanyteaze <tteaze@gmail.com>
2019-03-17 18:30:13 -0700

    connects SQLAlchemy to app

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6384802125ddc802d99de02ca7ea2fe3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 posts = [
     {
